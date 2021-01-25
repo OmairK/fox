f43b986e0b9204e1e5f7f0a5bc18b7e08b8d0989
Omair Khan <omairkhan064@gmail.com>
2021-01-25 14:49:17 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OmairK/fox
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/OmairK/fox/internal/server"
+)
+
+func main() {
+	service := ":8000"
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+
+	srv := server.NewServer(service)
+
+	select {
+	case <-sig:
+		srv.Stop()
+	}
+
+}
